f08ca022d2a09d205edbcef935f31ed406eb155b
Alex Mai <alexandertmai@gmail.com>
2018-02-28 02:41:15 -0800

    Running

--- src/xbox_ctl.py
+++ src/xbox_ctl.py
@@ -2,41 +2,41 @@
 import rospy
 from sensor_msgs.msg import Joy
 from serial import Serial
-import time
+import time, math
 
 #addr = "00:14:03:05:F1:F5"
-MAX_SPEED = 255
-DEAD_ZONE = 0.1
-MSG_RATE  = 10 #in Hz
-
-MSG_PER = 1/MSG_RATE
-
+MSG_RATE  = 3 #in Hz
+MSG_PER = 1./MSG_RATE
 last_message_send = 0
+MAX_TURNING_RADIUS = 20
 
-def deadzone(raw_value):
-    if abs(raw_value) < DEAD_ZONE:
-        return 0
-    return raw_value
+s = Serial("/dev/rfcomm0", 9600)
 
 def callback(msg):
+    global last_message_send
     if time.time() - last_message_send < MSG_PER:
         return
     last_message_send = time.time()
-    
-    turningRadius = -deadzone(msg.axes[0])
+
+    turningRadius = -msg.axes[0]
     sign = -1 if turningRadius < 0 else 1
-    turningRadius = sign*(4.1-4*abs(turningRadius))
-    forwardVel = 255*deadzone(msg.axes[1])
-    turningSpeed = 1 if abs(turningRadius) > 0 else 0
-    turningSpeed = (MAX_SPEED-abs(forwardVel))*turningSpeed/4
-    msg = b'%f, %f, %f' % (turningRadius, turningSpeed, forwardVel)
+    turningRadius = sign*(MAX_TURNING_RADIUS*(1.01-abs(turningRadius)))
+
+    forwardVel = 255*msg.axes[1]
+
+    vel_mag = math.sqrt(msg.axes[0]**2 + msg.axes[1]**2)
+    turningSpeed = (255*vel_mag-abs(forwardVel))/4
+
+    #turningSpeed = 1 if abs(turningRadius) > 0 else 0
+    msg = b'%f,%f,%f,\n' % (turningRadius, turningSpeed, forwardVel)
     print(msg)
-    #s.write(msg)
+    s.write(msg)
 
 def init():
     rospy.init_node("xbox_ctl", anonymous=True)
     rospy.Subscriber("/joy", Joy, callback)
     rospy.spin()
+    s.close()
 
 if __name__ == "__main__":
     init()

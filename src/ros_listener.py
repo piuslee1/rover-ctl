f77d3702cb6bb298e2018ae52e3ea05ad41a862c
Cyrus Cowley <cyficowley@gmail.com>
2019-10-20 16:30:38 -0700

    wrapped the messenger in ros

--- src/ros_listener.py
+++ src/ros_listener.py
@@ -1,7 +1,11 @@
+import rospy
 from tkinter import *
 import tkinter as tk
 import serial, time
 import messages
+
+from std_msgs.msg import Float64MultiArray
+
 
 buffer_count = 0
 CLEAR_BUFFER = 2
@@ -9,28 +13,17 @@ CLEAR_BUFFER = 2
 ser = serial.Serial("/dev/serial/by-id/usb-1a86_USB2.0-Serial-if00-port0", 115200)
 time.sleep(1)
 
-root = tk.Tk()
-maxs = 210
 
-v = Scale(root, from_=-maxs, to=maxs)
-v.set(0)
-v.pack()
-h = Scale(root, from_=-maxs, to=maxs)
-h.set(0)
-h.pack()
-
-def make_msg(speeds):
-    pass
-
-def proc():
+def dt_listener(msg):
     global buffer_count
-    speeds = [v.get(), 0, 0, h.get(), 0, 0]
+    x = msg.data[0]
+    y = msg.data[1]
+    left = y + x
+    right = y - x
+
+    speeds = [right, right, right, left, left, left]
     mess = messages.Message(messages.TARGET_SYSTEMS.DRIVE, speeds)
-    #  cmd = "#0#{}\n".format(",".join([str(s) for s in speeds]))
-    #  print(cmd)
-    #  ser.write(bytes(cmd, "UTF-8"))
     ser.write(b'a' + mess.serialize())
-    #  print(ser.readline(ser.inWaiting()))
     while ser.in_waiting:
         print(ser.readline())
     print("real hash: " +  str(mess.hash))
@@ -38,7 +31,8 @@ def proc():
         ser.reset_input_buffer()
         ser.reset_output_buffer()
     buffer_count += 1
-    root.after(200, proc)
 
-root.after(200, proc)
-root.mainloop()
+rospy.init_node('dt_listener')
+rospy.loginfo("started drive_train")
+test_arm_sub = rospy.Subscriber('drive_train', Float64MultiArray, dt_listener)
+rospy.spin()
--- src/test_messages.py
+++
@@ -1,6 +0,0 @@
-import serial
-from messages import Message
-
-serial_object = serial.Serial('/dev/ttyACM0',115200, timeout = 1)
-
-Message(6, [20,0,0,0,0,0], serial_object)

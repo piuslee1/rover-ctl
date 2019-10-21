45aa2b6aacb64180a5b7200b01440d28a1c6e3a7
Your Name <you@example.com>
2019-10-21 00:47:14 +0000

    changed to magnitude instead

--- src/ros_listener.py
+++ src/ros_listener.py
@@ -1,6 +1,4 @@
 import rospy
-from tkinter import *
-import tkinter as tk
 import serial, time
 import messages
 import math
@@ -14,16 +12,20 @@ CLEAR_BUFFER = 2
 ser = serial.Serial("/dev/serial/by-id/usb-1a86_USB2.0-Serial-if00-port0", 115200)
 time.sleep(1)
 
+rospy.init_node('dt_listener')
+rospy.loginfo("started drive_train")
+rate = rospy.Rate(10)
 
 def dt_listener(msg):
     global buffer_count
     x = msg.data[0]
     y = msg.data[1]
+    if x**2 + y**2 == 0:
+        return
 
-    left_max = max(abs((y + x) / math.sqrt(x**2 + y**2)), 1e-9) 
-    right_max = max(abs(y - x) / math.sqrt(x**2 + y**2), 1e-9) 
-    left = (y + x) / left_max 
-    right = (y - x) / right_max 
+    magnitude = max((abs(y) + abs(x)) / math.sqrt(x**2 + y**2), 1e-9) 
+    left = int(255*(y + x) / magnitude)
+    right = int(255*(y - x) / magnitude)
 
     speeds = [right, right, right, left, left, left]
     mess = messages.Message(messages.TARGET_SYSTEMS.DRIVE, speeds)
@@ -35,8 +37,7 @@ def dt_listener(msg):
         ser.reset_input_buffer()
         ser.reset_output_buffer()
     buffer_count += 1
+    rate.sleep()
 
-rospy.init_node('dt_listener')
-rospy.loginfo("started drive_train")
 test_arm_sub = rospy.Subscriber('drive_train', Float64MultiArray, dt_listener)
 rospy.spin()
